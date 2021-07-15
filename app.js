84d70a65d0dc40bc0e69cc733aab9cc44fd34f64
Antonio Aldridge <aldridge.developer@gmail.com>
2021-07-15 18:50:12 -0400

    Updated library with icons

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ const displayTickets = (tickets) => {
         .map((ticket) => {
             return `
             <div class="ticket">
-                <h2 onclick="addActiveClass(this)">${ticket.ticketName}</h2> 
+                <h2 onclick="addActiveClass(this)"><i class="${ticket.icon}"></i>${ticket.ticketName}</h2> 
                 <h3>Ticket Rating: ${ticket.ticket_rating}</h3>
                 <div class="template" title="Click to copy!"><span>${ticket.desc}</span><br><br><span class="copy-template">Click on the text to copy the template</span><br><br><textarea class="ticketTemplate" onclick="clickToCopy(this)">${ticket.template}</textarea></div><br>
             </div>
